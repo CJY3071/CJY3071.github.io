90236af61c2fe6d0bf82bb5b0e245ffaf2ca9f74
CJY3071 <1754823071@qq.com>
2019-05-28 15:36:11 +0800

    Site updated: 2019-05-28 15:36:08

---
+++ 飞机大战/offline.js
@@ -0,0 +1,25 @@
+﻿{
+	"version": 1558535838,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/背景-sheet0.png",
+		"images/飞机-sheet0.png",
+		"images/飞机-sheet1.png",
+		"images/飞机-sheet2.png",
+		"images/子弹-sheet0.png",
+		"images/敌机1-sheet0.png",
+		"images/敌机1-sheet1.png",
+		"images/栅栏-sheet0.png",
+		"images/重新开始-sheet0.png",
+		"images/开始界面-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
